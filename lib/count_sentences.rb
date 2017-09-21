c08ab9f5466e4335b3c13c4a5e6b66f89e3dd1ac
rebeccanewborn <rjn58@cornell.edu>
2017-09-21 18:21:23 -0400

    finished

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self[-1] == "."
   end
 
   def question?
-
+    self[-1] == "?"
   end
 
   def exclamation?
-
+    self[-1] == "!"
   end
 
   def count_sentences
-
+  #  self.sub("...",".").tr(".!","?").split.length
+    self.squeeze!
+    # binding.pry
+    arr = self.split(/[!?.]/)
+    arr.length
   end
 end
